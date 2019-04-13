225a64747bfe1c2551fabae955d2316378816eea
opari <opari@DESKTOP-7O97477.lan>
2019-04-13 23:25:41 +0100

    Implemented loading states into scenarios.

--- AutomatedAdventure/src/backend/Scenario.java
+++ AutomatedAdventure/src/backend/Scenario.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import json.JsonEntityArray;
+import json.JsonEntityString;
 import json.RestrictedJson;
 import json.restrictions.ElementRestriction;
 import json.restrictions.RoomRestriction;
 import json.restrictions.ScenarioRestriction;
+import json.restrictions.StateRestriction;
 
 public class Scenario
 {
@@ -19,7 +21,13 @@ public class Scenario
 	public Scenario(RestrictedJson<ScenarioRestriction> scenarioJson)
 	{
 		this.scenarioJson = scenarioJson;
-		
+		this.loadRooms();
+		this.loadElements();
+		this.loadStates();	
+	}
+	
+	private void loadRooms()
+	{
 		JsonEntityArray<RestrictedJson<RoomRestriction>> roomJsonArray = 
 				this.scenarioJson.getRestrictedJsonArray(ScenarioRestriction.ROOMS, RoomRestriction.class);
 		for (int i = 0; i < roomJsonArray.getLength(); i++)
@@ -27,7 +35,10 @@ public class Scenario
 			RestrictedJson<RoomRestriction> roomJson = roomJsonArray.getMemberAt(i);
 			this.rooms.add(new Room(roomJson));
 		}
-		
+	}
+	
+	private void loadElements()
+	{
 		JsonEntityArray<RestrictedJson<ElementRestriction>> elementJsonArray = 
 				this.scenarioJson.getRestrictedJsonArray(ScenarioRestriction.ELEMENTS, ElementRestriction.class);
 		
@@ -36,6 +47,17 @@ public class Scenario
 			RestrictedJson<ElementRestriction> elementJson = elementJsonArray.getMemberAt(i);
 			this.elements.add(new Element(elementJson, 1));
 		}
-		
+	}
+	
+	private void loadStates()
+	{
+		JsonEntityArray<RestrictedJson<StateRestriction>> stateJsonArray = 
+				this.scenarioJson.getRestrictedJsonArray(ScenarioRestriction.STATES, StateRestriction.class);
+		for (int i = 0; i < stateJsonArray.getLength(); i++)
+		{
+			RestrictedJson<StateRestriction> stateJson = stateJsonArray.getMemberAt(i);
+			JsonEntityString nameJson = stateJson.getJsonEntityString(StateRestriction.NAME);
+			this.states.put(nameJson.renderAsString(), new State(stateJson, 0));
+		}
 	}
 }
