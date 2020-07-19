880c3b49ed29995883c1bba9083bfb6b0957e58c
opari <opari@DESKTOP-7O97477.lan>
2020-07-19 19:34:51 +0100

    Changed choice box button appearances.

--- AutomatedAdventure/src/frontEnd/ChoiceBox.java
+++ AutomatedAdventure/src/frontEnd/ChoiceBox.java
@@ -1,6 +1,7 @@
 package frontEnd;
 
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -74,11 +75,18 @@ public class ChoiceBox extends JList<ChoiceEntry> implements ListSelectionListen
 		@Override
 		public Component getListCellRendererComponent(JList arg0, Object arg1, int arg2, boolean arg3, boolean arg4)
 		{	
+			Font font = new Font("arial", 1, 20);
 			if (arg1 == null)
-				return new JLabel("");
+			{
+				JLabel label = new JLabel("");
+				label.setFont(font);
+				return label;
+			}
 			
 			ChoiceEntry choiceEntry = (ChoiceEntry) arg1;
-			return new JLabel(choiceEntry.value);
+			JLabel label = new JLabel(choiceEntry.value);
+			label.setFont(font);
+			return label;
 		}
 		
 	}
